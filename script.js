f11b4b3d6651fe09ce721fab31a7a5d05c2de7fb
AndrePacolla <andreluizzpacolla@outlook.com>
2023-10-12 12:28:40 -0300

    adição de jquery , utilizando biblioteca slick

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 AOS.init();
+
+
+$(document).ready(function(){
+    $('.js-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            },
+          },
+        ],
+      });
+    });
